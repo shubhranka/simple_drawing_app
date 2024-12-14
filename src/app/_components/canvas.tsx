6aee46e7d194a263cbf8d126dac7838f52220aa6
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2024-12-15 00:55:13 +0530

    changed some imp

--- src/app/_components/canvas.tsx
+++ src/app/_components/canvas.tsx
@@ -117,7 +117,7 @@ export default function Canvas() {
 
     const getThing = async () => {
         setLoading(true);
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GOOGLE_API_KEY}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
